492ad730ed45a86025d850e4de94ca5293d755bb
Richard_zhao <richard_zhao93@163.com>
2021-09-18 13:12:39 +0800

    发布到 npm 前的准备: 修改版本号; 新建 index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import Button from './src/components/Button';
+import ButtonGroup from './src/components/ButtonGroup';
+import Svg from './src/components/Svg';
+
+export default {
+  Button,
+  ButtonGroup,
+  Svg
+}
