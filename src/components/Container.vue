afba2f853af316a108519061fc176109fa14d0e1
Richard_zhao <richard_zhao93@163.com>
2021-09-27 11:17:32 +0800

    优化代码

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -17,10 +17,8 @@ export default {
     // 通过 isVertical 变量控制
     // 存在则将 flex-direction 属性值设置为 row；
     // 反之，则设置为 column
-    this.$nextTick(() => {
-      this.$children.forEach(child => {
-        child.$options.name === 'HAside' ? (this.isVertical = true) : ''
-      })
+    this.$children.forEach(child => {
+      child.$options.name === 'HAside' ? (this.isVertical = true) : ''
     })
   }
 }
