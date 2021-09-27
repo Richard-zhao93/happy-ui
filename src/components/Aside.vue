45a9d899c2460675a73aba2ec856bd23b8cfcff8
Richard_zhao <richard_zhao93@163.com>
2021-09-27 09:30:24 +0800

    新增布局容器相关组件 Container Header Aside Main Footer

---
+++ src/components/Aside.vue
@@ -0,0 +1,9 @@
+<template> </template>
+
+<script>
+export default {
+  name: 'HAside'
+}
+</script>
+
+<style></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template> </template>
+
+<script>
+export default {
+  name: 'HFooter'
+}
+</script>
+
+<style></style>
